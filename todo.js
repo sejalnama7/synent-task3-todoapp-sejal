bf3eddf0297a6a81222e0da5efb2138cf5aff027
sejalnama7 <sejalnama7@gmail.com>
2026-04-30 01:06:49 +0530

    Render tasks with checkbox and delete button

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 window.onload = loadTasks;
 
+
 function addTask() {
   let input = document.getElementById("taskInput");
   let task = input.value;
@@ -15,4 +16,56 @@ function addTask() {
   saveTask(taskObj);
 
   input.value = "";
+}
+
+function createTask(taskObj) {
+  let li = document.createElement("li");
+
+  let leftDiv = document.createElement("div");
+  leftDiv.style.display = "flex";
+  leftDiv.style.alignItems = "center";
+  leftDiv.style.gap = "10px";
+
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = taskObj.completed;
+
+  let span = document.createElement("span");
+  span.innerText = taskObj.text;
+
+  
+  if (taskObj.completed) {
+    span.style.textDecoration = "line-through";
+  }
+
+
+  checkbox.onchange = function () {
+    if (checkbox.checked) {
+      span.style.textDecoration = "line-through";
+    } else {
+      span.style.textDecoration = "none";
+    }
+
+    updateTaskStatus(taskObj.text, checkbox.checked);
+  };
+
+  leftDiv.appendChild(checkbox);
+  leftDiv.appendChild(span);
+
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "delete";
+
+  delBtn.onclick = function () {
+    li.remove();
+    deleteTask(taskObj.text);
+  };
+
+  li.style.display = "flex";
+  li.style.justifyContent = "space-between";
+  li.style.alignItems = "center";
+
+  li.appendChild(leftDiv);
+  li.appendChild(delBtn);
+
+  document.getElementById("taskList").appendChild(li);
 }
